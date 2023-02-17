1d0c6a6a8ca46056d3ddfa33d3eca4340a9c0132
ceenaa <sina.moradi2003@yahoo.com>
2023-02-17 12:07:31 +0330

    fix: exam_time bug in json

--- api/models.py
+++ api/models.py
@@ -13,12 +13,13 @@ class Time(models.Model):
 
 
 class Course(models.Model):
+    id = models.ImageField
     name = models.CharField(max_length=100)
     teacher = models.CharField(max_length=100)
     code = models.CharField(max_length=100)
     group = models.CharField(max_length=100)
     class_times = models.ManyToManyField(Time, related_name='Course')
-    exam_time = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='exam')
+    exam_time = models.ForeignKey(Time, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- api/serializers.py
+++ api/serializers.py
@@ -8,8 +8,15 @@ class ClassTimeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class ForeignClassTimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Time
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
-    class_times = ClassTimeSerializer(many=True, read_only=True)
+    class_times = ClassTimeSerializer(many=True, read_only=True, )
+    exam_time = ForeignClassTimeSerializer(read_only=True)
 
     class Meta:
         model = Course
