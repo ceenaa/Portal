ad3094aa811269a2b7dd547ab19eb43e15fbc09f
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-02-23 22:20:00 +0330

    fix: Problem in rendering single-time courses.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,6 +3,7 @@ const barsBtn = document.querySelector(".navbar__right-icon-container");
 const menu = document.querySelector(".navbar__right-links");
 const checkboxContainer = document.querySelectorAll(".tables__table-checkbox");
 const table = document.querySelector(".tables__table-body");
+const refreshButton = document.querySelector(".refresh__btn");
 
 // Responsive Bars Icon:
 barsBtn.addEventListener("click", () => {
@@ -21,41 +22,54 @@ checkboxContainer.forEach((e) => {
 	});
 });
 
-// Fetch API:
-fetch("https://lilms.iran.liara.run/courses/")
-	.then((response) => response.json())
-	.then((data) => {
-		data.forEach((course) => {
-			table.insertAdjacentHTML(
-				"beforeend",
-				`
-            <tr class="tables__table-row">
-                <td class="tables__table-checkbox">
-                    <input class="tables__table-input" type="checkbox" />
-                </td>
-                <td class="tables__table-title">
-                    <span class="tables__table-title-text">${course.name}</span>
-                </td>
-                <td class="tables__table-code">
-                    <span class="tables__table-code-text">${course.code}</span>
-                </td>
-                <td class="tables__table-group">
-                    <span class="tables__table-group-text">${course.group}</span>
-                </td>
-                <td class="tables__table-teacher">
-                    <span class="tables__table-teacher-text">${course.teacher}</span>
-                </td>
-                <td class="tables__table-time1">
-                    <span class="tables__table-time1-text">${course.class_times[0].day}[${course.class_times[0].end_time}-${course.class_times[0].start_time}]</span>
-            </td>
-            <td class="tables__table-time2">
-                <span class="tables__table-time2-text">${course.class_times[1].day}[${course.class_times[1].end_time}-${course.class_times[1].start_time}]</span>
-            </td>
-            <td class="tables__table-exam">
-                <span class="tables__table-exam-text">${course.exam_time.day}[${course.exam_time.start_time}0-${course.exam_time.end_time}0]</span>
-            </td>
-        </tr>
-            `
-			);
+function getAllCourses() {
+	// Fetch API:
+	fetch("https://lilms.iran.liara.run/courses/")
+		.then((response) => response.json())
+		.then((data) => {
+            console.log(data);
+			data.forEach((course) => {
+				table.insertAdjacentHTML(
+					"beforeend",
+					`
+                        <tr class="tables__table-row">
+                            <td class="tables__table-checkbox">
+                                <input class="tables__table-input" type="checkbox" />
+                            </td>
+                            <td class="tables__table-title">
+                                <span class="tables__table-title-text">${course.name}</span>
+                            </td>
+                            <td class="tables__table-code">
+                                <span class="tables__table-code-text">${course.code}</span>
+                            </td>
+                            <td class="tables__table-group">
+                                <span class="tables__table-group-text">${course.group}</span>
+                            </td>
+                            <td class="tables__table-teacher">
+                                <span class="tables__table-teacher-text">${course.teacher}</span>
+                            </td>
+                            <td class="tables__table-time1">
+                                <span class="tables__table-time1-text">${course.class_times[0].day}[${course.class_times[0].end_time}-${course.class_times[0].start_time}]</span>
+                            </td>
+                            <td class="tables__table-time2">
+                                <span class="tables__table-time2-text">${course.class_times[1] === undefined ? "" : course.class_times[1].day}[${course.class_times[1] === undefined ? "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" : course.class_times[1].start_time}-${course.class_times[1] === undefined ? "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" : course.class_times[1].end_time}]</span>
+                            </td>
+                            <td class="tables__table-exam">
+                                <span class="tables__table-exam-text">${course.exam_time.day}[${course.exam_time.start_time}0-${course.exam_time.end_time}0]</span>
+                            </td>
+                        </tr>
+                    `
+				);
+			});
 		});
-	});
+}
+
+window.addEventListener("load", () => {
+	getAllCourses();
+});
+
+refreshButton.addEventListener("click", () => {
+    console.log('click');
+    table.innerHTML = ''
+    getAllCourses();
+});
