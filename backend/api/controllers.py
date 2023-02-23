6250f87b0276ec0f335eca80e79685a7f2499521
ceenaa <sina.moradi2003@yahoo.com>
2023-02-23 19:07:59 +0330

    fix: times and coherenec

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -45,8 +45,8 @@ def crawl(request):
 
         ct1 = Time.objects.create(
             day=extractDay(allTds[i + 8].text),
-            start_time=extractTime(allTds[i + 8].text)[1],
-            end_time=extractTime(allTds[i + 8].text)[0]
+            start_time=extractTime(allTds[i + 8].text)[0],
+            end_time=extractTime(allTds[i + 8].text)[1]
 
         )
 
@@ -59,16 +59,16 @@ def crawl(request):
         if allTds[i + 9].text != "":
             ct2 = Time.objects.create(
                 day=extractDay(allTds[i + 9].text),
-                start_time=extractTime(allTds[i + 9].text)[1],
-                end_time=extractTime(allTds[i + 9].text)[0]
+                start_time=extractTime(allTds[i + 9].text)[0],
+                end_time=extractTime(allTds[i + 9].text)[1]
             )
 
             cts.append(ct2)
         if allTds[i + 10].text != "":
             ct3 = Time.objects.create(
                 day=extractDay(allTds[i + 10].text),
-                start_time=extractTime(allTds[i + 10].text)[1],
-                end_time=extractTime(allTds[i + 10].text)[0]
+                start_time=extractTime(allTds[i + 10].text)[0],
+                end_time=extractTime(allTds[i + 10].text)[1]
             )
             cts.append(ct3)
 
@@ -90,25 +90,48 @@ def crawl(request):
     return JsonResponse({'message': 'crawl done'})
 
 
-def checkCoherence(c):
+def compareTime(t1, t2):
+    h1 = t1.split(":")[0]
+    m1 = t1.split(":")[1]
+    h2 = t2.split(":")[0]
+    m2 = t2.split(":")[1]
+    if h1 > h2:
+        return True
+    elif h1 == h2:
+        if m1 > m2:
+            return True
+        else:
+            return False
+    else:
+        return False
 
+
+# cct.start_time < ct.end_time <= cct.end_time
+
+def checkCoherence(c):
     chosenCourses = ChosenCourse.objects.all()
     if c in chosenCourses:
         return False
     for cc in chosenCourses:
 
         if cc.course.exam_time.day == c.exam_time.day:
-            if cc.course.exam_time.start_time <= c.exam_time.start_time < cc.course.exam_time.end_time:
+            if compareTime(cc.course.exam_time.start_time, c.exam_time.start_time) or \
+                    compareTime(c.exam_time.end_time, cc.course.exam_time.end_time) \
+                    or cc.course.exam_time.start_time == c.exam_time.start_time:
                 return False
-            if cc.course.exam_time.start_time < c.exam_time.end_time <= cc.course.exam_time.end_time:
+            if compareTime(cc.course.exam_time.end_time, c.exam_time.end_time) or \
+                    compareTime(c.exam_time.start_time, cc.course.exam_time.start_time) \
+                    or cc.course.exam_time.end_time == c.exam_time.end_time:
                 return False
 
         for cct in cc.course.class_times.all():
             for ct in c.class_times.all():
                 if cct.day == ct.day:
-                    if cct.start_time <= ct.start_time < cct.end_time:
+                    if compareTime(cct.end_time, ct.start_time) or compareTime(ct.end_time, cct.start_time)\
+                            or cct.start_time == ct.start_time:
                         return False
-                    if cct.start_time < ct.end_time <= cct.end_time:
+                    if compareTime(cct.end_time, ct.end_time) or compareTime(ct.end_time, cct.start_time)\
+                            or cct.end_time == ct.end_time:
                         return False
 
     return True
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,18 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+lu-1$(k7)w%_r%bottm-l9hk*i*4yw)113yf!=bgb*gp_bpb'
+SECRET_KEY = "*@eod5bow9-q17c_+pkuc*i*1eno8k_9wj)8y0sgqt3&oh1vg9"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'config.urls'
@@ -77,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -87,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -107,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -118,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
